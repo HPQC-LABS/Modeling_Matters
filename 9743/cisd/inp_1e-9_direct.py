32e9036a0cc6c719d429e3e53dd0aae909e32233
Nike Dattani <dattani.nike@gmail.com>
2023-01-04 18:00:18 -0800

    Added folder for 9743

---
+++ 9743/cisd/inp_1e-9_direct.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Author: Nike Dattani, nike@hpqc.org
+
+import numpy as np
+import pyscf
+from pyscf import gto, scf, ao2mo, fci,ci
+
+mol = pyscf.M(atom = 'Ne 0 0 0',basis = 'cc-pv5z',verbose=5,output='out_1e-9_direct.txt')
+mhf = scf.RHF(mol).run()
+mci = ci.CISD(mhf).set(conv_tol=1e-9,nroots=3)
+e, civec = mci.kernel()
