a66d33ea0d393f77beec7e72bbd2a72c9e445ce1
Nike Dattani <dattani.nike@gmail.com>
2023-01-04 18:57:55 -0800

    Added FCI folder to 9743. It works!

---
+++ 9743/fci/inp_1e-9_restarted.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# Author: Nike Dattani, nike@hpqc.org
+
+import numpy as np
+import pyscf
+from pyscf import gto, scf, ao2mo, fci,ci
+
+mol = pyscf.M(atom = 'Ne 0 0 0',basis = 'cc-pvDz',verbose=5,output='out_1e-9_restarted.txt')
+mhf = scf.RHF(mol).run()
+mci = fci.FCI(mhf).set(conv_tol=1e-9,nroots=3)
+e, civec = mci.kernel(ci0=np.load('civec.npy'))
+#np.save('civec.npy', civec)
